2fd49f244115544712532a0108053ed16bc6f32a
Jason Sherrick <jasonsherrick@gmail.com>
2024-04-27 00:36:26 -0500

    Update SCSS files for BEM compliance and refactor color variables

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,8 +1,1 @@
-import { SmartCam } from "./smartcam.js";
-
-const div = document.querySelector('div.smart-cam') as HTMLDivElement;
-const overlay = div.querySelector('#overlay') as HTMLDivElement;
-const button = div.querySelector('button') as HTMLButtonElement;
-const video = div.querySelector('video') as HTMLVideoElement;
-
-const smartCam = new SmartCam({ video, button, overlay, minConfidence: 0.5, maxObjects: 3});
+// TODO: this
